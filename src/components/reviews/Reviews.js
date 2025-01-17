eb2b72777b4acc3200c48477d20d33cf45eff510
denbas <denbasxlll@gmail.com>
2025-01-17 16:53:08 +0100

    Final Soldi v1.006

--- src/components/reviews/Reviews.js
+++ src/components/reviews/Reviews.js
@@ -31,7 +31,8 @@ function Reviews({setRef}) {
     
     const updateSizes = useCallback(() => {
         setValue(getSlideValue());
-        // setClickedDotId(0);
+        
+        setClickedDotId(0);
         if (sliderRef.current) {
             setSliderWidth(sliderRef.current.getBoundingClientRect().width);
         }
@@ -57,17 +58,15 @@ function Reviews({setRef}) {
     },[clickedDotId, SliderWidth])
 
     const handlers = useSwipeable({
-        onSwipedLeft: (eventData) => {
-            if (Math.abs(eventData.deltaX) < 50) { // Проверяем модуль для любого направления
-              setClickedDotId((prev) => (prev < dots.length - 1 ? prev + 1 : 0)); // Переход вперед
-            }
+        onSwipedLeft: () => {
+            setClickedDotId((prev) => (prev < dots.length - 1 ? prev + 1 : 0))
           },
-          onSwipedRight: (eventData) => {
-            if (Math.abs(eventData.deltaX) < 50) { // Проверяем модуль для любого направления
-              setClickedDotId((prev) => (prev > 0 ? prev - 1 : dots.length - 1)); // Переход назад
-            }
-          },
-
+        onSwipedRight: () => {
+        setClickedDotId((prev) => (prev > 0 ? prev - 1 : dots.length - 1))
+        },
+        preventDefaultTouchmoveEvent: true, 
+        trackTouch: true,
+        trackMouse: false,
     });
     
     const dots = useMemo(() => {
